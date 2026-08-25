54f656f41a5df8efa68b98d7ca0edc848ec0d290
Agustin Saravia <agustin.saravia@combinedratio.com>
2026-08-25 19:14:29 -0300

    feature: type addition for Remotive response

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+
+export interface RemotiveResponse { 
+    jobs: RemotiveJobPost[]
+}
+export interface RemotiveJobPost { 
+    id: number
+    url: string
+    title: string
+    company_name: string
+    company_logo: string
+    category: string
+    tags: string[]
+    job_type: string
+    publication_date: string
+    candidate_required_location: string
+    salary: string
+    description: string
+}
